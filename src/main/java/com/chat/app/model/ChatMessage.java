e6137dc1adefbfec0943a36ddf4980a9149157a5
vikrantl30 <vikrant.deshmukh21@vit.edu>
2026-04-17 12:03:32 +0530

    Initial commit

---
+++ src/main/java/com/chat/app/model/ChatMessage.java
@@ -0,0 +1,12 @@
+package com.chat.app.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ChatMessage {
+    private Long id;
+    private String sender;
+    private String content;
+}
